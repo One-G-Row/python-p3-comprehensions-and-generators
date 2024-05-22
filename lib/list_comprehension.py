552d4693ae13a54ed8fb4fbba18ae224a74e2bc7
student <kmac.kimaru93@gmail.com>
2024-05-22 19:18:52 +0300

    adding comprehensions and generators

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    return [item for item in num_list if item % 2 == 0]
+
+return_evens([0, 1, 3, 5, 7, 8, 9])
 
 def make_exclamation(sentence_list):
-    pass
+    return[word + '!' for word in sentence_list]
+
+make_exclamation(["Hello", "I'm doing great", "Python is fun"])
